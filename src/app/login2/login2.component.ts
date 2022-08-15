bd79f1856326950984a986b97b60053b1533e521
RuTz92 <franco.angiolini@gmail.com>
2022-08-15 08:20:09 -0300

    Implementacion Back con Front

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginUsuario } from '../model/login-usuario';
+import { AuthService } from '../service/auth.service';
+import { TokenService } from '../service/token.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login2',
   templateUrl: './login2.component.html',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Login2Component implements OnInit {
 
-  constructor() { }
+  isLogged: boolean = false;
+  isLogginFail = false;
+  loginUsuario!: LoginUsuario;
+  nombreUsuario!: string;
+  password!: string;
+  roles: string[] = [];
+  errMsj!: string;
+
+  constructor(private tokenService: TokenService, private authService: AuthService,
+     private Router: Router) {
+
+   }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+      this.isLogginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+  }
+
+  onLogin(): void {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(data => {
+      this.isLogged = true;
+      this.isLogginFail = false;
+      this.tokenService.setToken(data.token);
+      this.tokenService.setUsername(data.nombreUsuario);
+      this.tokenService.setAuthorities(data.authorities);
+      this.roles = data.authorities;
+      this.Router.navigate(['']);
+    }, err => {
+      this.isLogged= false;
+      this.isLogginFail = true;
+      this.errMsj = err.error.mensaje;
+      console.log(this.errMsj)
+    }
+      );
   }
 
 }
