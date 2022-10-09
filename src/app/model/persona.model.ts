f34349a4b4932d9f75458ad88746dcca031a9d69
RuTz92 <franco.angiolini@gmail.com>
2022-10-09 07:49:18 -0300

    Traigo datos del Back para ABout

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -3,10 +3,16 @@ export class persona{
     nombre:string;
     apellido:string;
     img:string;
+    titulo1:string;
+    titulo2:string;
+    descripcion:string;
 
-    constructor(nombre: string, apellido: string, img: string) {
+    constructor(nombre: string, apellido: string, img: string, titulo1: string, titulo2: string, descripcion: string) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img;
+        this.titulo1= titulo1;
+        this.titulo2= titulo2;
+        this.descripcion = descripcion;
     }
 }
